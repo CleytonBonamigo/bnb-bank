8436bf8986048133cfae93f895ee10b8693bb891
Cleyton Bonamigo <cleytonbonamigo@gmail.com>
2023-08-24 18:37:54 -0300

    Change some configurations to run the project.

--- start.sh
+++ start.sh
@@ -6,23 +6,29 @@ echo "Deploying application ..."
 docker compose up -d
 
 # Install dependencies based on lock file
+echo "Running composer installment of dependencies"
 docker exec bnb-bank-php composer install --prefer-dist --optimize-autoloader
 
-docker exec bnb-bank-php cp -n .env.example .env
-docker exec bnb-bank-php php artisan key:generate
+# Install NPM Dependencies
+echo "Installing NPM dependencies"
+docker exec bnb-bank-php npm install
+# Compile Frontend
+echo "Compiling frontend stuff"
+docker exec bnb-bank-php npm run build
+
+echo "Sleeping for 30 seconds to MySQL initiate completely"
+sleep 30
 
 # Migrate database
+echo "Migrating Databases"
 docker exec bnb-bank-php php artisan migrate --database=bnb-bank
 docker exec bnb-bank-php php artisan migrate --database=bnb-bank-test
 # Seeds the database
+echo "Seeding database"
 docker exec bnb-bank-php php artisan db:seed --database=bnb-bank
 
-# Install NPM Dependencies
-docker exec bnb-bank-php npm install
-# Compile Frontend
-docker exec bnb-bank-php npm run build
-
 # Run tests to make sure it's ok to deploy
+echo "Running PEST tests"
 docker exec bnb-bank-php ./vendor/bin/pest
 
 echo "Application deployed!"
